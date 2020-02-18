8e49d7c389bdac1891a9a54a7bbed39726b0d4b7
mkovarky <mikhail.kovarsky@gmail.com>
2020-02-18 14:14:52 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args ) {
+        int balance = 2_000_000_000;
+        int money_tranfer = 500_000_000;
+        int current_balance = balance + money_tranfer;
+        System.out.println(current_balance);
+    }
+
+}
