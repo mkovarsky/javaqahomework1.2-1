985823418bd57c9ed174d1b8bad0b635848b0a7d
mkovarky <mikhail.kovarsky@gmail.com>
2020-02-18 15:07:04 +0300

    Main.java modified + report.md added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args ) {
         int balance = 2_000_000_000;
-        int money_tranfer = 500_000_000;
-        int current_balance = balance + money_tranfer;
+        int money_transfer = 500_000_000;
+        int current_balance = balance + money_transfer;
         System.out.println(current_balance);
     }
 
